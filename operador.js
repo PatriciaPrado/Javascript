f00af9f6c35e0059d1e1c33de08e7f7edfca10e7
Patricia Prado <patricia.acprado@senacsp.edu.br>
2019-05-06 14:35:21 -0300

    Operadores de comparação

---
+++ operador.js
@@ -0,0 +1,21 @@
+/* Operadores de comparação
+   ------------------------
+
+Igual (==) -> verifica se os valores são iguais
+
+Idêntico (===) -> verifica se os valores são iguais e do mesmo tipo
+
+Diferente (!=) -> verifica se os valores são diferentes
+
+Não idêntico (!==) ->  verifica se os valores são diferentes e os tipos também
+
+Menor (<) ->  verifica se o valor da esquerda é menor do que o valor da direita
+
+Maior (<) ->  verifica se o valor da direita é menor do que o valor da esquerda
+
+Menor ou igual (<) ->  verifica se o valor da esquerda é menor do que o valor da direita ou igual
+
+Maior ou igual (<) ->  verifica se o valor da direita é menor do que o valor da esquerda ou igual
+*/
+
+
