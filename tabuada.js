a2d2576d28a55f5df6625d964900211e9277dfc4
Patricia Prado <patricia.acprado@senacsp.edu.br>
2019-05-08 14:15:17 -0300

    Tabuada

--- tabuada.js
+++ tabuada.js
@@ -1,18 +1,15 @@
-let valor1 = 1;
-let tab = 0;
-
 // for(let x = 1; x <= 10; x++){
 //     for(let x = 1; x <= 10; valor1++){
 //         tab = valor1 * x;
 //         console.log(`${x} * ${valor1} = ${tab}`);
 // }
 
-
 for(let x = 1; x <= 10; x++)
 {
     for(let valor1 = 1; valor1 <= 10; valor1++)
     {
+        let tab = x * valor1
         console.log(`${x} * ${valor1} = ${tab}`);
-}
+    }
 }
 
